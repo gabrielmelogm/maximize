ba685b143e8b6fb54dd4796739a74fab0421e590
Gabriel <melogoncalvesbiel@gmail.com>
2022-06-10 14:27:09 -0300

    feat(components/main/form): finalizado a estilizacao do componente form

--- src/components/Main/Form/index.tsx
+++ src/components/Main/Form/index.tsx
@@ -1,18 +1,24 @@
 import styles from "./styles.module.scss"
 import { FiSend } from "react-icons/fi"
+import { IoIosArrowDown } from "react-icons/io"
 
 export function Form() {
   return (
     <div className={styles.content}>
       <form className={styles.form}>
-        <label htmlFor="input-contact-type" className={styles.labelSelect}>
-          Seu contato é relacionado a:
-        </label>
-        <select className={styles.selectInput} name="contact_type" id="input-contact-type">
-          <option value="Imprensa">Imprensa</option>
-          <option value="Fãs">Fãs</option>
-          <option value="Empresário(a)">Empresário(a)</option>
-        </select>
+        <div className={styles.contentInput}>
+          <label htmlFor="input-contact-type" className={styles.labelSelect}>
+            Seu contato é relacionado a:
+          </label>
+          <select className={styles.selectInput} name="contact_type" id="input-contact-type" required>
+            <option value="Imprensa">Imprensa</option>
+            <option value="Fãs">Fãs</option>
+            <option value="Empresário(a)">Empresário(a)</option>
+          </select>
+          <label htmlFor="input-contact-type" className={styles.iconSelect}>
+            <IoIosArrowDown />
+          </label>
+        </div>
 
         <div className={styles.contentGroupInputs}>
 
@@ -41,11 +47,22 @@ export function Form() {
             <textarea id="message" className={styles.inputMessage} name="message" placeholder="Digite aqui..." rows={5} required />
           </div>
         </div>
-        <div className={styles.contentCheckbox}>
-          <input className={styles.inputCheckbox} name="recaptcha" id="recaptcha" type="checkbox" checked={true} />
-          <label className={styles.labelCheckbox} htmlFor="recaptcha">Não sou um robô</label>
+        <div className={styles.contentFooterForm}>
+          <div className={styles.contentCheckbox}>
+            <div className={styles.checkbox}>
+              <input className={styles.inputCheckbox} name="recaptcha" id="recaptcha" type="checkbox" />
+              <label className={styles.labelCheckbox} htmlFor="recaptcha">Não sou um robô</label>
+            </div>
+          </div>
+          <button className={styles.buttonSubmit} type="submit">
+            <span>
+              Enviar
+            </span>
+            <span className={styles.sendIcon}>
+              <FiSend size={24} />
+            </span>
+          </button>
         </div>
-        <button>Enviar </button>
       </form>
     </div>
   )
